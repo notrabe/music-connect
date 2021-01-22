c4edf70bc33ebe50a95930ecb852c51242b30b76
Evan Barton <notrabe1011@gmail.com>
2021-01-21 23:38:25 -0500

    made user model

--- server.js
+++ server.js
@@ -3,12 +3,24 @@ const connectDB = require('./config/db')
 
 const app = express()
 
+//import routers
+const usersRouter = require('./routes/api/users')
+const postsRouter = require('./routes/api/posts')
+const authRouter = require('./routes/api/auth')
+const profileRouter = require('./routes/api/profile')
+
 //connect database
 connectDB()
 
 app.get('/', (req, res) => {
     res.send('Hello')
 })
+
+//define routers
+app.use('/api/users', usersRouter)
+app.use('/api/auth', authRouter)
+app.use('/api/profile', profileRouter)
+app.use('/api/posts', postsRouter)
 
 const PORT = process.env.PORT || 5000
 
