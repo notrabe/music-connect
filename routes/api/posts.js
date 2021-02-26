16f2a753037622d6d1cdbc5cf166595492495222
Evan Barton <notrabe1011@gmail.com>
2021-02-26 17:03:39 -0500

    delete post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -89,4 +89,39 @@ router.get('/:id', auth, async (req, res) => {
     }
 })
 
+//@route    DELETE api/posts/:id
+//@desc     Delete all posts
+//@access   Private
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+
+        if (!post) {
+            return res.status(404).json({
+                message: 'post not found'
+            })
+        }
+
+        // check user
+        if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({
+                msg: 'User not authorized'
+            })
+        }
+
+        await post.remove()
+
+        res.json({
+            msg: 'post removed'
+        })
+    } catch (err) {
+        if (err.kind == 'ObjectId') {
+            return res.status(404).json({
+                message: 'post not found'
+            })
+        }
+        res.status(500).send('Server Error')
+    }
+})
+
 module.exports = router
