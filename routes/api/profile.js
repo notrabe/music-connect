3e796806fdc1c700928c250f188b1f911bf1371f
Evan Barton <notrabe1011@gmail.com>
2021-02-26 14:38:45 -0500

    delete profile and user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -92,7 +92,7 @@ router.post('/', auth, check('instruments', 'instruments is required').notEmpty(
 
 //@route    GET api/profile
 //@desc     Get all profiles
-//@access   Private
+//@access   Public
 router.get('/', async (req, res) => {
     try {
         const profiles = await Profile.find().populate('user', ['name', 'avatar'])
@@ -122,5 +122,23 @@ router.get('/user/:user_id', async (req, res) => {
         res.status(500).send('server error')
     }
 })
+
+//@route    DELETE api/profile
+//@desc     Delete profile, user, & posts
+//@access   Private
+router.delete('/', auth, async (req, res) => {
+    
+    try {
+        // remove profile
+        await Profile.findOneAndRemove({user: req.user.id})
+        // remove user
+        await User.findOneAndRemove({_id: req.user.id})
+        res.json({msg: "User deleted"})
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('server error')
+    }
+})
+
 
 module.exports = router
