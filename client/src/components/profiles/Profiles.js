10390108e57b18120d28437c978421805fd010b6
Evan Barton <notrabe1011@gmail.com>
2021-02-28 21:22:04 -0500

    did more stuff but I can't even remember what tbh

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -24,7 +24,7 @@ const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
           <div className="profiles">
             {profiles.length > 0 ? (
               profiles.map((profile) => (
-                <ProfileItem key={profile._id} profile={profile} />
+                <ProfileItem key={profile.user._id} profile={profile} />
               ))
             ) : (
               <h4>No profiles found.</h4>
