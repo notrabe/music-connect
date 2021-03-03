30e60c7950f5b3a69cd71afa1472bf501fcf8887
Evan Barton <notrabe1011@gmail.com>
2021-03-03 10:26:15 -0500

    added comments

--- client/src/components/post/CommentItem.js
+++ client/src/components/post/CommentItem.js
@@ -3,11 +3,13 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Moment from 'react-moment';
+import { deleteComment } from '../../actions/post';
 
 const CommentItem = ({
   postId,
   comment: { _id, text, name, avatar, user, date },
   auth,
+  deleteComment,
 }) => {
   return (
     <div class="post bg-white p-1 my-1">
@@ -22,6 +24,15 @@ const CommentItem = ({
         <p class="post-date">
           Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
         </p>
+        {!auth.loading && user === auth.user._id && (
+          <button
+            onClick={(e) => deleteComment(postId, _id)}
+            type="button"
+            className="btn btn-danger"
+          >
+            <i className="fas fa-times"></i>
+          </button>
+        )}
       </div>
     </div>
   );
@@ -31,10 +42,11 @@ CommentItem.propTypes = {
   postId: PropTypes.number.isRequired,
   comment: PropTypes.object.isRequired,
   auth: PropTypes.object.isRequired,
+  deleteComment: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   auth: state.auth,
 });
 
-export default connect(mapStateToProps, {})(CommentItem);
+export default connect(mapStateToProps, { deleteComment })(CommentItem);
