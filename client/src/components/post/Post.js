b64b1baa0af180b7697c425715f564bef77daa15
Evan Barton <notrabe1011@gmail.com>
2021-03-03 09:55:54 -0500

    comment form

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import PostItem from '../posts/PostItem';
+import CommentForm from '../post/CommentForm';
 import { getPost } from '../../actions/post';
 import { Link } from 'react-router-dom';
 
@@ -21,6 +22,7 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
             Back to Posts
           </Link>
           <PostItem post={post} showActions={false} />
+          <CommentForm postId={post._id} />
         </>
       )}
     </div>
