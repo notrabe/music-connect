4a332aa3069638c77bdab6ae36edbe594c906eca
Evan Barton <notrabe1011@gmail.com>
2021-03-04 15:33:08 -0500

    prepare for deployment

--- server.js
+++ server.js
@@ -1,30 +1,37 @@
-const express = require('express')
-const connectDB = require('./config/db')
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
 
-const app = express()
+const app = express();
 
 //import routers
-const usersRouter = require('./routes/api/users')
-const postsRouter = require('./routes/api/posts')
-const authRouter = require('./routes/api/auth')
-const profileRouter = require('./routes/api/profile')
+const usersRouter = require('./routes/api/users');
+const postsRouter = require('./routes/api/posts');
+const authRouter = require('./routes/api/auth');
+const profileRouter = require('./routes/api/profile');
 
 //connect database
-connectDB()
+connectDB();
 
 //init middleware
-app.use(express.json({extended: false}))
-
-app.get('/', (req, res) => {
-    res.send('Hello')
-})
+app.use(express.json({ extended: false }));
 
 //define routers
-app.use('/api/users', usersRouter)
-app.use('/api/auth', authRouter)
-app.use('/api/profile', profileRouter)
-app.use('/api/posts', postsRouter)
+app.use('/api/users', usersRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/profile', profileRouter);
+app.use('/api/posts', postsRouter);
 
-const PORT = process.env.PORT || 5000
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
-app.listen(PORT, () => console.log(`Server running on ${PORT}`))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
